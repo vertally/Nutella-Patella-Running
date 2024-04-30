78496e977e04c774d80b3cb3df24e7a121487834
vertally <vgtaleon@gmail.com>
2024-04-30 15:10:11 -0400

    Tests for app user service WIP.

---
+++ server/running/src/test/java/org/example/domain/AppUserServiceTest.java
@@ -0,0 +1,61 @@
+package org.example.domain;
+
+import org.example.data.AppUserRepository;
+import org.example.models.AppUser;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.dao.DataAccessException;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+
+@SpringBootTest
+class AppUserServiceTest {
+
+    @Autowired
+    AppUserService service;
+
+    @MockBean
+    AppUserRepository repository;
+
+    @Test
+    void shouldFindByUsername() throws DataAccessException {
+        AppUser expected = makeAppUser(1);
+        when(repository.findAppUserByUsername("NutellaPatella")).thenReturn(expected);
+
+        UserDetails actual = service.loadUserByUsername("NutellaPatella");
+
+        assertEquals(expected.getUsername(), actual.getUsername());
+    }
+
+    @Test
+    void shouldNotFindByUsernameWhenUsernameDoesNotExist() throws DataAccessException {
+        AppUser expected = makeAppUser(1);
+        when(repository.findAppUserByUsername("NutellaPatella")).thenReturn(expected);
+        when(repository.findAppUserByUsername("CoachNutellaPatella")).thenReturn(null);
+
+        UserDetails actual = service.loadUserByUsername("CoachNutellaPatella");
+
+        assertNull(actual);
+    }
+
+    // HELPER METHODS
+    private AppUser makeAppUser(int appUserId) {
+        AppUser appUser = new AppUser(
+                appUserId,
+                "nutellapatella@gmail.com",
+                "NutellaPatella",
+                "jellylegs2024",
+                true,
+                List.of("runner")
+        );
+
+        return appUser;
+    }
+
+}
